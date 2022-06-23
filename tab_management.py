fc2a4b6c3376f9fba042e1fc0a331c9c45ff4736
Kate Nickoriuk <61067904+knickoriuk@users.noreply.github.com>
2022-06-23 18:52:09 -0300

    Restructured Database Format
    - Now the database allows for negative values. This allows for an increase in performance when running `$who_owes_me`, since it only needs to check one dictionary entry instead of all of them. 
    - `query()` now performs the function of `find_who_owes_me()` but now you must specify the mode as `inquire` or `who_owes_me`.
    - The above changes have eliminated the need for `cross_check()` and `find_who_owes_me()`
    - `pay()` will now remove users from the database if there are no entries for them, to help with performance and database size.
    - Added some functions to assist in debugging and restructuring the database, which may be removed later.
    
    Instead of the database looking like this:
              `{"frank": {"bob":30, "susie":44}, "bob": {}, "susie": {"bob":19}}`
    it will look like:
              `{"frank": {"bob":30, "susie":44}, "bob": {"frank":-30, "susie":-19}, "susie": {"bob":19, "frank":-44}}`
    (except with user ID values instead of names)

--- tab_management.py
+++ tab_management.py
@@ -1,12 +1,16 @@
 from replit import db
 
-# db has a key for each user, containing a dictionary of user:amount
-# recording total amount owed to each user.
-# eg. db[user1][user2] returns the amount that user1 owes user2
+# db has a key for each user, containing a dictionary of user:amount recording total amount owed to each user. 
+# eg. frank owes bob $30, susie owes bob $19, frank owes susie $44
+# db = {"frank": {"bob":30, "susie":44}, 
+#       "bob":   {"frank":-30, "susie":-19}, 
+#       "susie": {"bob":19, "frank":-44}}
 
-def pay(user_paying, user_receiving, amt):
-    ''' Marks that the user `user_paying` has paid an
-    amount `amt` to `user_receiving`. '''
+def pay(user_paying, user_receiving, amount_paid):
+    ''' (string, string, float) -> Bool
+    Marks that the user `user_paying` has paid an amount `amount_paid` to
+    `user_receiving`. Returns True if successful, False otherwise.
+    '''
     user_paying = str(user_paying)
     user_receiving = str(user_receiving)
 
@@ -16,83 +20,103 @@ def pay(user_paying, user_receiving, amt):
     if user_receiving not in db[user_paying].keys():
         return False
 
-    db[user_paying][user_receiving] -= amt
+    amount_owed = db[user_paying][user_receiving] - amount_paid
 
     # if paid in full, remove from tab list
-    if db[user_paying][user_receiving] <= 0:
+    if amount_owed < 0.01: # Prevent floating point errors from lingering
         db[user_paying].pop(user_receiving)
+        db[user_receiving].pop(user_paying)
 
+        # Also: remove user from database if there are no entries in db[user]
+        if not db[user_paying]:
+            db.pop(user_paying)
+        if not db[user_receiving]:
+            db.pop(user_receiving)
+        
+    # Otherwise update it to reflect new amount
+    else:
+        db[user_paying][user_receiving] = amount_owed
+        db[user_receiving][user_paying] = -amount_owed
     return True
 
-def add_tab(user_owed, user_to_pay, amt):
-    ''' Records that `user_to_pay` now owes `user_owed` the amount `amt`. '''
+def add_tab(user_owed, user_to_pay, amount_owed):
+    ''' (string, string, float) -> Bool
+    Records that `user_to_pay` now owes `user_owed` the amount `amount_owed`. 
+    Returns True if successful, False otherwise.
+    '''
     user_to_pay = str(user_to_pay)
     user_owed = str(user_owed)
 
     if user_to_pay == user_owed:
         return False
 
+    # Initialize people in database if not yet there
     if user_to_pay not in db.keys():
         db[user_to_pay] = {}
+    if user_owed not in db.keys():
+        db[user_owed] = {}
 
+    # Add positive balance to `user_owed`
     if user_owed not in db[user_to_pay].keys():
-        db[user_to_pay][user_owed] = amt
+        db[user_to_pay][user_owed] = amount_owed
     else:
-        db[user_to_pay][user_owed] += amt
-    cross_check(user_owed, user_to_pay)
+        db[user_to_pay][user_owed] += amount_owed
+        
+    # Add negative balance to `user_to_pay`
+    if user_to_pay not in db[user_owed].keys():
+        db[user_owed][user_to_pay] = -amount_owed
+    else:
+        db[user_owed][user_to_pay] -= amount_owed
+    
     return True
 
-def query(user):
-    ''' Determines how much this user owes others. '''
+def query(user, mode=None):
+    ''' (string, string) -> dictionary of {str: float}
+    Determines how much this user owes others and is owed by others.
+        - a negative balance implies the user IS OWED that amount,
+        - a positive balance implies the user OWES that amount 
+        
+    Specify mode="inquire" to find only positive balances
+    or mode="who_owes_me" to find only negative balances (which will
+    be made positive). The default mode is None.
+    '''
     user = str(user)
-    if user not in db.keys():
+    if user not in db.keys():   
         return {}
-    return db[user]
+    if mode != "inquire" and mode != "who_owes_me":
+        return db[user]
+    else:
+        new_db = {}
+        for other_user in db[user].keys():
+            
+            amount = db[user][other_user]
+            if amount > 0 and mode == "inquire" : new_db[other_user] = amount 
+            if amount < 0 and mode == "who_owes_me" : new_db[other_user] = -amount 
+          
+        return new_db
 
-def cross_check(user1, user2):
-    ''' Checks between the two users to see if any owed balances
-    cancel out, and removes that balance. '''
-    user1 = str(user1)
-    user2 = str(user2)
+def _clear_database():
+	''' Resets the database entirely. '''
+	db.clear()
+	return
 
-    if user1 not in db.keys() or user2 not in db.keys():
-        return
-    user1_tab = query(user1)
-    user2_tab = query(user2)
+def _get_entire_database():
+    '''For debug purposes. '''
+    return db
 
-    if user1 not in user2_tab.keys() or user2 not in user1_tab.keys():
-        return
-    balance1 = user1_tab[user2] # amount user1 owes user2
-    balance2 = user2_tab[user1] # amount user2 owes user1
-
-    if abs(balance1-balance2) < 0.01 or user1 == user2:
-        db[user1].pop(user2)
-        db[user2].pop(user1)
-
-    elif balance1 > balance2:
-        # Subtract balance2 from balance1, set as user1's tab
-        # set user2's tab to 0
-        db[user1][user2] = balance1 - balance2
-        db[user2].pop(user1)
-
-    elif balance1 < balance2:
-        # Subtract balance1 from balance2, set as user2's tab
-        # set user1's tab to 0
-        db[user2][user1] = balance2 - balance1
-        db[user1].pop(user2)
+def _convert_db():
+    '''A function to be ran once, converting the old format of db to
+    the new format. '''
+    for user1 in db.keys():
+        for user2 in db[user1].keys():
+            if user2 not in db.keys():
+                db[user2] = {}
+            db[user2][user1] = -db[user1][user2]
     return
 
-def clear_database():
-    ''' Resets the database entirely. '''
-    db.clear()
+def _clean_db():
+    '''A function to be ran once, cleaning empty database entries. '''
+    for user in db.keys():
+        if not db[user]:
+            db.pop(user)
     return
-
-def find_who_owes_me(user):
-    ''' Given a user, find who owes this person and how much.
-    Returns list of tuple [(user1, amount), ...] '''
-    user = str(user)
-    results = []
-    for other in db.keys():
-        if user in db[other].keys():
-            results.append((other, db[other][user]))
-    return results
